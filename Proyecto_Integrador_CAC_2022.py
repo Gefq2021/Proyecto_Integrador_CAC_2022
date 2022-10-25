73d8d5826ead1eb8b148d9bd6b5a1b30e3afa38e
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-10-24 22:01:16 -0300

    Etapa 2

--- Proyecto_Integrador_CAC_2022.py
+++ Proyecto_Integrador_CAC_2022.py
@@ -10,8 +10,8 @@ Una universidad desea crear un programa para contabilizar los cursos que tiene c
 Para ello se debe realizar primero una aplicación de consola la cual debe solicitar el nombre de un alumno y la cantidad de cursos en la que se encuentra inscripto.
 Estos dos valores deben almacenarse como una lista de dos elementos (el nombre y la cantidad de cursos como un número entero) en una lista de alumnos.
 """
-num = 0
-alumnos = []
+
+alumnos = {}
 
 # Funcion que comprueba si se ingreso un numero entero
 def es_numero(valor):
@@ -21,24 +21,28 @@ def es_numero(valor):
     return int(valor)
 
 
-while num != 3:
-    alumno = []
+while True:
     print('''Ingreser el número de la operación que desea realizar:
 1- Añadir un alumno a la listas.
 2- Ver la lista de alumnos.
-3- Salir.''')
+3. Ver la cantidad de cursos de un alumno.
+4- Salir.''')
     num = es_numero(input())
     match num:
         case 1:
             nombre = input("Ingrese el nombre: ")
-            alumno.append(nombre)
-            cant = int(input("Ingrese la cantidad de materias cursos: "))
-            alumno.append(cant)
-            alumnos.append(alumno)
+            cant = es_numero(input("Ingrese la cantidad de materias cursos: "))
+            alumnos.update({nombre: cant})
         case 2:
-            for a in alumnos:
-                print(f"Nombre: {a[0]}. Cantidad de Materias Inscriptas {a[1]}")
+            for nombre, cursos in alumnos.items():
+                print(f"Nombre: {nombre}. Cantidad de Materias Inscriptas {cursos}")
         case 3:
+            alumno = input("Ingrese el Nombre del Alumno: ")
+            if alumno in alumnos:
+                print(f"Nombre: {alumno}. Cursos: {alumnos[alumno]}")
+            else:
+                print(f"El alumno {alumno}, No se encuentra.")
+        case 4:
             print("¡Gracias por útilizar el programa!")
             break
         case other:
